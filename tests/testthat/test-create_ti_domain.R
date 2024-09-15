0324e638dd620f6d454980300cc4d87d12035f88
janardanswami <saijanardhanswami@mail.com>
2024-09-15 22:33:14 +0530

    deleted few unwanted files

--- tests/testthat/test-create_ti_domain.R
+++ tests/testthat/test-create_ti_domain.R
@@ -1,45 +1,43 @@
 # test_create_ti_domain.R
-library(testthat)
-library(autoTDD)
 
-test_that("create_ti_domain works with PDF method", {
-  study_id <- "STUDY001"
-  method <- "pdf"
-  pdf_path <- system.file("extdata", "Temp_Protocol_v1.pdf", package = "autoTDD")
-  incl_range <- 6:9
-  excl_range <- 9:16
-  incl_section <- "4.1.1"
-  excl_section <- "4.1.2"
-  end_section <- "4.2"
-  output_dir <- tempdir()
 
-  expect_error(create_ti_domain(
-    study_id = study_id,
-    method = method,
+
+###Tests cases written by sai
+
+test_that("Valid PDF method with correct inputs", {
+  pdf_path <- system.file("extdata", "Protocol.pdf", package = "autoTDD")
+
+  result <- create_ti_domain(
+    study_id = "STUDY001",
+    method = "pdf",
     pdf_path = pdf_path,
-    incl_range = incl_range,
-    excl_range = excl_range,
-    incl_section = incl_section,
-    excl_section = excl_section,
-    end_section = end_section,
-    output_dir = output_dir
-  ), NA)
-})
-
-test_that("create_ti_domain works with API method", {
-  study_id <- "STUDY001"
-  method <- "api"
-  nct_id <- "NCT00000419"
-  output_dir <- tempdir()
-
-  ti_domain <- create_ti_domain(
-    study_id = study_id,
-    method = method,
-    nct_id = nct_id,
-    output_dir = output_dir
+    incl_range = 41:44,
+    excl_range = 44:46,
+    incl_section = "4.1.1",
+    excl_section = "4.1.2",
+    end_section = "4.2"
   )
 
-  expect_true(is.data.frame(ti_domain))
-  expect_true(nrow(ti_domain) > 0)
-  expect_true(file.exists(file.path(output_dir, paste0(study_id, "_TI.xlsx"))))
+  expect_type(result, "list")
 })
+
+
+
+
+test_that("Throw an error if any of the arguments are missing", {
+  pdf_path <- system.file("extdata", "Protocol.pdf", package = "autoTDD")
+
+  expect_error(
+    create_ti_domain(
+      study_id = "STUDY001",
+      method = "pdf",
+      pdf_path = pdf_path,
+      incl_range = NULL,  # Set this to NULL to simulate a missing argument
+      excl_range = 44:46,
+      incl_section = "4.1.1",
+      excl_section = "4.1.2",
+      end_section = "4.2"
+    )
+  )
+})
+
